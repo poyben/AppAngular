e4b4dd68de0793beafb2640f7105aa9be038ee15
poyben <poybenglez@gmail.com>
2024-04-11 13:20:42 +0100

    Implementado error handler

--- appangularfront/src/app/services/auth/login.service.ts
+++ appangularfront/src/app/services/auth/login.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { LoginRequest } from './loginRequest';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, catchError, throwError } from 'rxjs';
 import { User } from './user';
 
 @Injectable({
@@ -12,8 +12,21 @@ export class LoginService {
   constructor(private http: HttpClient) { }
   
   login(credentials:LoginRequest):Observable<User>{
-    return this.http.get<User>("./assets/data.json")
+    return this.http.get<User>("./assets/data.json").pipe(
+      catchError(this.handleError)
+    )
   
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if(error.status ===0){
+      console.error("Ha habido un error", error.error);
+    }else{
+      console.error("Código de estado del Backend: ", error.error);
+    }
+    return throwError(()=> new Error("Ha ocurrido un error, por favor intentelo de nuevo"));
+  
+
   }
 
 }
