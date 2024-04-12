e073017a06624ac6127c7bad82f11559c2e1b2ee
poyben <poybenglez@gmail.com>
2024-04-12 11:37:42 +0100

    securityconfig permisos

--- apirestjwtang/src/main/java/com/rubengs/apirestjwt/user/UserRepository.java
+++ apirestjwtang/src/main/java/com/rubengs/apirestjwt/user/UserRepository.java
@@ -11,6 +11,6 @@ public interface UserRepository extends JpaRepository<User,Integer>{
 	Optional<User> findByUsername(String username);
 	
 	@Modifying()
-	@Query("update User u set u.firstname=:firstname, u.lastname=:lastname,u.contry=:country where u.id=:id")
+	@Query("update User u set u.firstname=:firstname, u.lastname=:lastname,u.country=:country where u.id=:id")
 	void updateUser(@Param(value="id") Integer id, @Param(value="firstname") String firstname, @Param(value="lastname") String lastname, @Param(value="country") String country);
 }
