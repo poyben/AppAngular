09ec903aeecf8f48c2caec1f1537a4ccb83fce00
poyben <poybenglez@gmail.com>
2024-04-12 10:14:55 +0100

    userresponse

---
+++ apirestjwtang/src/main/java/com/rubengs/apirestjwt/user/UserResponse.java
@@ -0,0 +1,16 @@
+package com.rubengs.apirestjwt.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserResponse {
+	
+	String message;
+	
+}
