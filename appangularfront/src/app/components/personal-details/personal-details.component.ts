80acd42de4f0653efaa9267f22a789e5a73fa866
poyben <poybenglez@gmail.com>
2024-04-12 14:05:51 +0100

    cambiado componente datos

--- appangularfront/src/app/components/personal-details/personal-details.component.ts
+++ appangularfront/src/app/components/personal-details/personal-details.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { User } from '../../services/auth/user';
 import { environment } from '../../../environments/environments';
 import { UserService } from '../../services/user/user.service';
+import { last } from 'rxjs';
+import { Validators,FormBuilder } from '@angular/forms';
+import { LoginService } from '../../services/auth/login.service';
 
 @Component({
   selector: 'app-personal-details',
@@ -11,8 +14,17 @@ import { UserService } from '../../services/user/user.service';
 export class PersonalDetailsComponent {
   errorMessage:String = "";
   user?:User;
+  userLoginOn:boolean = false;
+  editMode:boolean = false;
 
-  constructor(private userService:UserService){
+  registerForm=this.formBuilder.group({
+    id:[""],
+    firstname:["",[Validators.required]],
+    lastname:["",[Validators.required]],
+    password:["",Validators.required]
+  });
+
+  constructor(private userService:UserService, private formBuilder: FormBuilder, private loginService:LoginService) { 
     this.userService.getUser(environment.userId).subscribe({
       next:(userData)=>{
         this.user = userData;
