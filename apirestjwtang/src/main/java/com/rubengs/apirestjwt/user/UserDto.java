848d34449adf3d758c934c25cc5864e66d48771b
poyben <poybenglez@gmail.com>
2024-04-12 10:18:04 +0100

    userdto

---
+++ apirestjwtang/src/main/java/com/rubengs/apirestjwt/user/UserDto.java
@@ -0,0 +1,20 @@
+package com.rubengs.apirestjwt.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDto {
+
+	int id;
+	String username;
+	String firstName;
+	String lastName;
+	String country;
+	
+}
