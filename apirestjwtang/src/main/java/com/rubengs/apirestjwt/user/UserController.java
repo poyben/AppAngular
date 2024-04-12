6bd4f29abc34fc2de63a710b02ef2d02c8542b93
poyben <poybenglez@gmail.com>
2024-04-12 11:42:08 +0100

    permisos crossorigin

--- apirestjwtang/src/main/java/com/rubengs/apirestjwt/user/UserController.java
+++ apirestjwtang/src/main/java/com/rubengs/apirestjwt/user/UserController.java
@@ -1,6 +1,7 @@
 package com.rubengs.apirestjwt.user;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -13,6 +14,7 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping(value="/api/v1/user")
 @RequiredArgsConstructor
+@CrossOrigin(origins= {"http://localhost:4200"})
 public class UserController {
 	
 	private final UserService userService;
