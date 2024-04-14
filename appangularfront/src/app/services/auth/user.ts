025b5ceea7408181db3a516520dfefb8907c51cd
poyben <poybenglez@gmail.com>
2024-04-14 11:37:53 +0100

    Ajustado user.ts

--- appangularfront/src/app/services/auth/user.ts
+++ appangularfront/src/app/services/auth/user.ts
@@ -1,7 +1,7 @@
 export interface User {
     id: number;
     username: string;
-    lastname?: string;
-    firstname?: string;
+    lastname: string;
+    firstname: string;
     country?:string;
 }
