7693024e4a539aeef537f9d849bf63274b5f3032
poyben <poybenglez@gmail.com>
2024-04-14 13:44:41 +0100

    hecho register service

---
+++ appangularfront/src/app/services/auth/register.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { BehaviorSubject, Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { User } from './user';
+import { environment } from '../../../environments/environments';
+import { RegisterRequest } from './registerRequest';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RegisterService {
+
+  currentUserLoginOn: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  currentUserData: BehaviorSubject<String> =new BehaviorSubject<String>("");
+
+  constructor(private http: HttpClient) { 
+    this.currentUserLoginOn=new BehaviorSubject<boolean>(sessionStorage.getItem("token")!=null);
+    this.currentUserData=new BehaviorSubject<String>(sessionStorage.getItem("token") || "");
+  }
+
+  register(registerRequest: RegisterRequest): Observable<any> {
+    return this.http.post<any>(environment.urlHost + 'auth/register', registerRequest).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      console.error('Se ha producido un error ', error.error);
+    } else {
+      console.error('Backend retornó el código de estado ', error);
+    }
+    return throwError(() => new Error('Algo falló. Por favor intente nuevamente.'));
+  }
+}
